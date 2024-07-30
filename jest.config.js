2bef33e418eba180711e7163bf00230773e7fbbe
Anton Liashkevich <antonl@jvl.ca>
2024-07-30 16:36:31 -0400

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testRegex: '/__tests__/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.{d.ts}',
+  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@library1/(.*)$': '<rootDir>/library-1/src/$1',
+    '^@library2/(.*)$': '<rootDir>/library-2/src/$1',
+    '^@odk/(.*)$': '<rootDir>/src/$1',
+  },
+};
